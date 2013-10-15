270489728d9c4391a7be8b945432534401d68e0e
Matthew Ricks <matthew@sixricks.com>
2013-10-14 18:53:52 -0600

    Debugging Project 2

--- src/GeometryDescriptor.java
+++ src/GeometryDescriptor.java
@@ -1,14 +1,37 @@
 
 public class GeometryDescriptor {
 	
-	int index;
+	Object object = null;
+	int index = -1;
+	public static final GeometryDescriptor NULL_DESCRIPTOR = new GeometryDescriptor();
 	
-	public GeometryDescriptor(int index) { this.index = index; }
+	public GeometryDescriptor() { }
+	public GeometryDescriptor(Object object, int index) {
+		this.object = object;
+		this.index = index;
+	}
 
+	@Override
+	public boolean equals(Object other) {
+		if(other instanceof GeometryDescriptor) {
+			GeometryDescriptor gd = (GeometryDescriptor)other;
+			if(index != gd.index) return false;
+			else if(object == null && gd.object == null) return true;
+			else if(object == null || gd.object == null) return false;
+			else if(!object.equals(gd.object)) return false;
+			else return true;
+		}
+		return false;
+	}
+	
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Index: " + index);
+		if(object == null) sb.append("Null");
+		else if(object instanceof NetworkConnection) sb.append("Connection");
+		else if(object instanceof NetworkNode) sb.append("Node");
+		else if(object instanceof String) sb.append("Character");
+		sb.append(" (" + index + ')');
 		return sb.toString();
 	}
 }
--- src/NetworkView.java
+++ src/NetworkView.java
@@ -13,9 +13,11 @@ import javax.swing.JPanel;
 public class NetworkView extends JPanel implements MouseListener {
 	
 	NetworkModel model;
+	GeometryDescriptor descriptor = new GeometryDescriptor();
 	
 	public NetworkView(NetworkModel model) {
 		this.model = model;
+		this.addMouseListener(this);
 	}
 	
 	@Override
@@ -63,36 +65,42 @@ public class NetworkView extends JPanel implements MouseListener {
 			
 			g.drawLine(x1, y1, x2, y2);
 		}
+		
+		if (!descriptor.equals(GeometryDescriptor.NULL_DESCRIPTOR)) g.drawString(descriptor.toString(), 5, 20);
     }
 	
-	
-	
-	public GeometryDescriptor pointGeometry(Point mouseLoc) throws Exception
-	{
+	public GeometryDescriptor pointGeometry(Point mouseLoc) {
 		final int ERROR = 5;
 		for(int i = model.nConnections() - 1; i >= 0; i--) {
+			// Get pieces
 			NetworkConnection c = model.getConnection(i);
 			NetworkNode n1 = null, n2 = null;
-			for(int j = 0; n1 == null || n2 == null; j++) {
+			for(int j = 0; (n1 == null || n2 == null) && j < model.nNodes(); j++) {
 				NetworkNode n = model.getNode(j);
 				if(n.getName() == c.node1) n1 = n;
 				else if(n.getName() == c.node2) n2 = n;
 			}
+			if (n1 == null || n2 == null) continue;
+			
+			// Get corners
+			FontMetrics fm = getFontMetrics(getFont());
 			Point p1 = new Point(), p2 = new Point();
 			p1.setLocation(n1.getX(), n1.getY());
-			Font.g;
 			switch(c.side1) {
-				case Bottom: p1.setLocation(p1.getX(), p1.getY() - );// TODO: Finish
-					break;
-				case Left:
-					break;
-				case Right:
-					break;
-				case Top:
-					break;
-				default: throw new Exception();
+				case Bottom: p1.setLocation(p1.getX(), p1.getY() - fm.getHeight()); break;
+				case Left: p1.setLocation(p1.getX() - fm.stringWidth(n1.getName()), p1.getY()); break;
+				case Right: p1.setLocation(p1.getX() + fm.stringWidth(n1.getName()), p1.getY()); break;
+				case Top: p1.setLocation(p1.getX(), p1.getY() + fm.getHeight()); break;
 			}
 			p2.setLocation(n2.getX(), n2.getY());
+			switch(c.side1) {
+				case Bottom: p2.setLocation(p2.getX(), p2.getY() - fm.getHeight()); break;
+				case Left: p2.setLocation(p2.getX() - fm.stringWidth(n2.getName()), p2.getY()); break;
+				case Right: p2.setLocation(p2.getX() + fm.stringWidth(n2.getName()), p2.getY()); break;
+				case Top: p2.setLocation(p2.getX(), p2.getY() + fm.getHeight()); break;
+			}
+			
+			// Get box
 			Point min = new Point(), max = new Point();
 			min.setLocation(Math.min(p1.getX(), p2.getX()), Math.min(p1.getY(), p2.getY()));
 			max.setLocation(Math.max(p1.getX(), p2.getX()), Math.max(p1.getY(), p2.getY()));
@@ -102,18 +110,49 @@ public class NetworkView extends JPanel implements MouseListener {
 			else if(mouseLoc.getY() > max.getY() + ERROR) continue;
 			else if(Math.abs((p2.getY() - p1.getY()) * mouseLoc.getX() + (p1.getX() - p2.getX()) * mouseLoc.getY() -
 					(p2.getY() - p1.getY()) * p1.getX() - (p1.getX() - p2.getX()) * p1.getY()) <= ERROR) {
-				return new GeometryDescriptor(i);
+				return new GeometryDescriptor(c, i);
 			}
 		}
 		for(int i = model.nNodes() - 1; i >= 0; i--) {
+			NetworkNode n = model.getNode(i);
+
+			// Get outer box
+			FontMetrics fm = getFontMetrics(getFont());
+			Point min = new Point(), max = new Point();
+			min.setLocation(n.getX() - fm.stringWidth(n.getName()), n.getY() - fm.getHeight());
+			max.setLocation(n.getX() + fm.stringWidth(n.getName()), n.getY() + fm.getHeight());
 			
+			if(Math.pow((mouseLoc.getX() - n.getX()) / fm.stringWidth(n.getName()), 2) + 
+					Math.pow((mouseLoc.getY() - n.getY()) / fm.getHeight(), 2) <= 1) {
+				
+				// Bounds check
+				if(mouseLoc.getX() < min.getX()) continue;
+				else if(mouseLoc.getX() > max.getX()) continue;
+				else if(mouseLoc.getY() < min.getY()) continue;
+				else if(mouseLoc.getY() > max.getY()) continue;
+				
+				// Get inner box
+				min.setLocation(n.getX() - fm.stringWidth(n.getName()) / 2, n.getY() - fm.getHeight() / 2);
+				max.setLocation(n.getX() + fm.stringWidth(n.getName()) / 2, n.getY() + fm.getHeight() / 2);
+				if(mouseLoc.getX() > min.getX() && mouseLoc.getX() < max.getX() &&
+						mouseLoc.getY() > min.getY() && mouseLoc.getY() < max.getY()) {
+					for(int j = 0, size = fm.stringWidth(n.getName().substring(0, 1)); j < n.getName().length();
+							size = fm.stringWidth(n.getName().substring(0, ++j + 1))) {
+						if (min.getX() + size > mouseLoc.getX()) return new GeometryDescriptor(n.getName(), j);
+					}
+				}
+				else return new GeometryDescriptor(n, i);
+			}
 		}
-		return null;
+		return new GeometryDescriptor();
 	}
 	
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		System.out.println(pointGeometry(e.getPoint()));
+		GeometryDescriptor gd = pointGeometry(e.getPoint());
+		if(gd.equals(GeometryDescriptor.NULL_DESCRIPTOR)) return;
+		else descriptor = gd;
+		repaint();
 	}
 
 	@Override
